10c342dccef1370a3de6ab9b5572f321340a8c6b
Talos-Edurobots <talos.edurobots@gmail.com>
2025-02-28 17:20:25 +0200

    Update AutonomousV3.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/AutonomousV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/AutonomousV3.java
@@ -104,7 +104,7 @@ public class AutonomousV3 extends LinearOpMode {
         telemetry.addData("Status", "Running");
         telemetry.update();
 
-        otosDrive(10, 0, 0, 2);      // small move forward and right away from wall
+        otosDrive(0, 50, 0, 2);      // small move forward and right away from wall
 //        otosDrive(18, 2, 0, 2);     // forward and push sample into net zone
 //        otosDrive(0, 24, 0, 2);     // backup and move away from wall
 //        otosDrive(-87, 24, 0, 4);   // backup straight
