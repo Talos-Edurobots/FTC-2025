fc562aa7fa11ac9fa37f29b245f38589640972a3
ManosFps1pt <manos.dragasakis@gmail.com>
2025-03-22 16:43:23 +0200

    changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Arm {
+    private DcMotor arm;
+    Arm(DcMotor arm) {
+        this.arm = arm;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Constants.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public class Constants {
+    public static final int ARM_TICKS_PER_DEGREE = 0;
+}
